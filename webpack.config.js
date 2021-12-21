48df15a17bb57bb83a968a6b56ec2a25fadd4ca7
Dav Clark <dav.clark@noteable.io>
2021-12-21 14:39:12 -0700

    Updates from André

--- webpack.config.js
+++ webpack.config.js
@@ -14,16 +14,26 @@ module.exports = {
   resolve: {
     // Add ".ts" and ".tsx" as resolvable extensions.
     extensions: ['.ts', '.tsx', '.js', '.jsx', '.css'],
-    alias: {
-      'apache-arrow': path.resolve(__dirname, 'node_modules/apache-arrow/Arrow.esnext.min.js'),
-      // Doesn't build?
-      // 'apache-arrow': path.resolve(__dirname, 'node_modules/@apache-arrow/ts/Arrow.ts'),
-    },
+    // The below is unnecessary if we set the rule below for mjs files.
+    // alias: {
+    //   'apache-arrow': path.resolve(__dirname, 'node_modules/apache-arrow/Arrow.esnext.min.js'),
+    //   // Doesn't build?
+    //   // 'apache-arrow': path.resolve(__dirname, 'node_modules/@apache-arrow/ts/Arrow.ts'),
+    // },
   },
   module: {
     rules: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: "ts-loader" },
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      },
+      {
+        // This seems to mess with devtools - which now wants to find every individual library!
+        // But this is a way to make arrow work without the alias above (which feels more hacky / brittle)
+        test: /\.m?js$/,
+        resolve: { fullySpecified: false },
+      },
     ],
   }
 };
